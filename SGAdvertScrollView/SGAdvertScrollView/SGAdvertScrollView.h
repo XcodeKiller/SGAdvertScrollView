1db076fcc77ea8256969d16e17ec45c6dbccc405
低调的魅力 <993234418@qq.com>
2017-03-09 00:02:58 +0800

    SGAdvertScrollView
    一个无限轮播广告图,点击跳转页面框架

---
+++ SGAdvertScrollView/SGAdvertScrollView/SGAdvertScrollView.h
@@ -0,0 +1,19 @@
+//
+//  SGAdvertScrollView.h
+//  AdvertScrollView
+//
+//  Created by lihaohao on 2017/3/8.
+//  Copyright © 2017年 低调的魅力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAdvertScrollView : UIView
+/*!
+ * @abstract                return instance
+ * @param imagesArray       图片URL数组
+ * @param timeInterval      自动轮播时间(秒) 默认为5秒
+ * @param advertSelectBlock 点击图片回调block
+ */
++ (instancetype)advertScrollViewFrame:(CGRect)frame imagesArray:(NSArray *)imagesArray timeInterval:(NSTimeInterval)timeInterval advertSelectBlock:(void(^)(int selectIndex))advertSelectBlock;
+@end
---
+++ SGAdvertScrollView/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  SGAdvertScrollView
+//
+//  Created by lihaohao on 2017/3/8.
+//  Copyright © 2017年 低调的魅力. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "SGAdvertScrollView.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *imageArray = [NSMutableArray arrayWithCapacity:8];
+    for (int i = 0; i < 8; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"%d.jpg",i+1];
+        [imageArray addObject:imageName];
+    }
+    
+    SGAdvertScrollView *advertScrollView = [SGAdvertScrollView advertScrollViewFrame:self.view.bounds imagesArray:imageArray timeInterval:5 advertSelectBlock:^(int selectIndex) {
+        NSLog(@"selectIndex:%d",selectIndex);
+    }];
+    [self.view addSubview:advertScrollView];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
